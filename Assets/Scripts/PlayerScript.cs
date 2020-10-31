33eef53797ed3148bf9b1fd8327187299ffd889e
Susanna Perez <peres90@knights.ucf.edu>
2020-10-31 13:36:38 -0400

    Challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,13 +11,40 @@ public class PlayerScript : MonoBehaviour
 
     public Text score;
 
+    public Text winText;
+
+    public Text livesText;
+
+    public AudioSource musicSource;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioClip musicClipThree;
+
+    public Animator anim; 
+
     private int scoreValue = 0;
+
+    private int livesValue = 3;
+
+    private bool facingRight = true;
+
+  
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Potions: " + scoreValue.ToString();
+        winText.text = "";
+        livesText.text = "Lives: " + livesValue.ToString();
+
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -31,6 +58,34 @@ public class PlayerScript : MonoBehaviour
         {
             Application.Quit();
         }
+
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+
+
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+
+        anim.SetInteger("State", 1);
+        anim.SetInteger("State", 0);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -38,8 +93,40 @@ public class PlayerScript : MonoBehaviour
         if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Potions: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector2(200.0f, 0.0f);
+                livesValue = 3;
+                livesText.text = "Lives: " + livesValue.ToString();
+            }
+        }
+
+        if (scoreValue == 8)
+        {
+            winText.text = "You Win! Created by Susanna Perez";
+
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            musicSource.loop = false;
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            livesText.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+            
+        }
+        if (livesValue == 0)
+        {
+            winText.text = "You lose! Created by Susanna Perez";
+            Destroy(gameObject, musicClipThree.length);
+           
+            musicSource.clip = musicClipThree;
+            musicSource.Play();
+            musicSource.loop = false;
         }
 
     }
@@ -51,7 +138,20 @@ public class PlayerScript : MonoBehaviour
             if (Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                anim.SetInteger("State", 1);
+            }
+            else
+            {
+                anim.SetInteger("State", 0);
             }
         }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
     }
 }
